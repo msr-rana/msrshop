a90e414e75c3a0625f1b1fe4321147b065670a04
msr-rana <61286387+msr-rana@users.noreply.github.com>
2021-06-14 14:23:31 +0600

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import react from 'react'
+import './App.css';
+
+function App() {
+  const Product = [
+    Mobile
+    Food
+    spors
+
+  ]
+  return (
+    <div className="App">
+      <h1>Welcome to msr.com</h1>
+      <Product>{Mobile}</Product>
+      
+    </div>
+  );
+}
+
+export default App;
